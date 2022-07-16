4a250ed65ab063067963432147c824dca387d4a4
Avishak Chakroborty <aurgho1998@gmail.com>
2022-07-16 14:46:32 +0600

    Time: 35 ms (42.21%), Space: 16.8 MB (98.75%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int>mp;
+        // map<int,int>mp;
         int val = nums[0];
-        for(int i = 0;i<nums.size();i++){
-            mp[nums[i]]++;
+        for(int i = 1;i<nums.size();i++){
+            val ^= nums[i];
         }
         
-        for(int i = 0;i<nums.size();i++){
-            if(mp[nums[i]]==1){
-                val = nums[i];
-                break;
-            }
-        }
+        
         
         return val;
     }
